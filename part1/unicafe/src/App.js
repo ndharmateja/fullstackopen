7d848c605f7460e1d953270c65f79b1e9db8daa4
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-14 08:05:44 +0530

    Exercise 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,6 +13,7 @@ const App = () => {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
--- part1/unicafe/src/Statistics.js
+++ part1/unicafe/src/Statistics.js
@@ -3,9 +3,11 @@ const Statistics = ({ good, neutral, bad }) => {
     (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
 
   const getPositivePercentage = () => (100 * good) / (good + neutral + bad)
-  return (
+
+  return good + neutral + bad === 0 ? (
+    <div>No feedback given</div>
+  ) : (
     <>
-      <h1>statistics</h1>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
