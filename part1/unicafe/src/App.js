c08654c23b3e6873575743734e904bd676f8a70b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-14 07:53:57 +0530

    Exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,17 +6,25 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const getAverage = () =>
+    (good * 1 + neutral * 0 + bad * -1) / (good + neutral + bad)
+
+  const getPositivePercentage = () => (100 * good) / (good + neutral + bad)
+
   return (
-    <>
+    <div>
       <h1>give feedback</h1>
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-    </>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+      <div>all {good + neutral + bad}</div>
+      <div>average {getAverage()}</div>
+      <div>positive {getPositivePercentage()}%</div>
+    </div>
   )
 }
 
