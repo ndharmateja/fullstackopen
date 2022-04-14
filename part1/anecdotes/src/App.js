22f6115cff2a7d04eb4e5a7b5006481c864d837a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-14 09:14:20 +0530

    Exercise 1.14

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -27,9 +27,12 @@ const App = () => {
 
   return (
     <>
+      <h1>Anecdote of the day</h1>
       <div>{anecdotes[selected]}</div>
       <button onClick={voteCurrentlySelected}>vote</button>
       <button onClick={selectRandomAnecdote}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[votes.indexOf(Math.max(...votes))]}</div>
     </>
   )
 }
