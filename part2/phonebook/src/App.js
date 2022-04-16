fcbfbba52eb07720273e71f4fce6d82527a37e0d
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-04-16 09:07:59 +0530

    Exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -4,18 +4,27 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }])
   const [newName, setNewName] = useState('')
 
+  const addNewName = (e) => {
+    e.preventDefault()
+    setPersons(persons.concat({ name: newName }))
+    setNewName('')
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addNewName}>
         <div>
-          name: <input />
+          name:{' '}
+          <input onChange={(e) => setNewName(e.target.value)} value={newName} />
         </div>
         <div>
           <button type='submit'>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div key={person.name}>{person.name}</div>
+      ))}
     </div>
   )
 }
