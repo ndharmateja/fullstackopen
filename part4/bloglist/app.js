89d17979021a4240441933c0bb357f7244b78fec
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-09 01:13:10 +0530

    Refactor into app.js

---
+++ part4/bloglist/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const cors = require('cors')
+const Blog = require('./models/blog')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs)
+  })
+})
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog.save().then((result) => {
+    response.status(201).json(result)
+  })
+})
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,29 +1,9 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
+const app = require('./app')
 const mongoose = require('mongoose')
 require('dotenv').config()
-const Blog = require('./models/blog')
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 const PORT = 3003
 app.listen(PORT, () => {
