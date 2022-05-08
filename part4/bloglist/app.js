3df4c3cc0d2ff3d054c71ee7d8a080d1e1cb9795
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-09 01:19:57 +0530

    Refactor into app.js

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
 const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = process.env.MONGODB_URI
+mongoose.connect(mongoUrl)
 
 const app = express()
 app.use(cors())
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,9 +1,5 @@
 const app = require('./app')
-const mongoose = require('mongoose')
 require('dotenv').config()
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl)
 
 const PORT = 3003
 app.listen(PORT, () => {
