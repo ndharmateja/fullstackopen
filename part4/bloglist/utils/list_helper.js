d31892bf5a226f9ff9a4976c71acfed6c46a51ad
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-09 15:09:16 +0530

    Exercises 4.4 and 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,3 +1,13 @@
 const dummy = (_blogs) => 1
 
-module.exports = { dummy }
+const totalLikes = (blogs) =>
+  blogs.reduce((total, blog) => total + blog.likes, 0)
+
+const favoriteBlog = (blogs) =>
+  blogs.length === 0
+    ? undefined
+    : blogs.reduce((prev, current) =>
+        prev.likes > current.likes ? prev : current
+      )
+
+module.exports = { dummy, totalLikes, favoriteBlog }
