50386a83d477e6676d271ce1fc54c8dbe97b1378
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-23 08:16:10 +0530

    Add test to check number of blogs in db

---
+++ part4/bloglist/tests/blogs_api.test.js
@@ -0,0 +1,30 @@
+const supertest = require('supertest')
+const app = require('../app')
+const { initialBlogs } = require('./helper')
+const Blog = require('../models/blog')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+  // Delete the existing blogs
+  await Blog.deleteMany({})
+
+  // Create promises to create blogs
+  const createBlogPromises = initialBlogs
+    .map((blog) => new Blog(blog))
+    .map((blog) => blog.save())
+
+  // Execute all promises
+  await Promise.all(createBlogPromises)
+})
+
+test('check number of blogs returned', async () => {
+  const response = await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+
+  const blogs = response.body
+  expect(blogs).toHaveLength(initialBlogs.length)
+  expect(blogs.map((blog) => blog.title)).toContainEqual(initialBlogs[0].title)
+})
