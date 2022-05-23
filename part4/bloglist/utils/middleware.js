c21b2984d12b41a80d101b1e2b27e1c75bd4b986
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-23 09:41:47 +0530

    Exercise 4.12

--- part4/bloglist/utils/middleware.js
+++ part4/bloglist/utils/middleware.js
@@ -1,5 +1,18 @@
+const { info } = require('./logger')
+
 const unknownRoute = (_request, response, _next) => {
   response.status(404).end()
 }
 
-module.exports = { unknownRoute }
+const errorHandler = (error, request, response, next) => {
+  const { name, message } = error
+  switch (name) {
+    case 'ValidationError':
+    case 'CastError':
+      return response.status(400).send({ error: message })
+  }
+  info(error)
+  next(error)
+}
+
+module.exports = { unknownRoute, errorHandler }
