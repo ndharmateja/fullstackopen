ef0de48b36e753d21c4fed0d429bc4cc31c336dc
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-24 10:16:26 +0530

    Exercise 4.18

---
+++ part4/bloglist/controllers/login.js
@@ -0,0 +1,25 @@
+const User = require('../models/user')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+const loginUser = async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await User.find({ username })
+  if (!user) {
+    return response.status(400).json({ error: 'invalid username' })
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.passwordHash)
+  if (!isPasswordCorrect) {
+    return response.status(400).json({ error: 'invalid password' })
+  }
+
+  const userForToken = { username, id: user._id }
+  const token = jwt.sign(userForToken, process.env.SECRET)
+
+  return response.json({ username, token, name: user.name })
+}
+
+module.exports = { loginUser }
