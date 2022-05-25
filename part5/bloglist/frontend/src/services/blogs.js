e5bd4933d3c7f84673ae0a46fa6dab8956006870
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-25 18:45:16 +0530

    Add token to blog creation

--- part5/bloglist/frontend/src/services/blogs.js
+++ part5/bloglist/frontend/src/services/blogs.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+const setToken = (newToken) => (token = `bearer ${newToken}`)
+
 const getAll = () => {
   const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+  return request.then((response) => response.data)
 }
 
-export default { getAll }
+const createBlog = async ({ title, author, url }) => {
+  const config = { headers: { Authorization: token } }
+  const response = await axios.post(baseUrl, { title, author, url }, config)
+  return response.data
+}
+
+export default { getAll, createBlog, setToken }
