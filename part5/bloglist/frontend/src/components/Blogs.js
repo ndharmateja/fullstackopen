62385b07ca38de12109314b0fb62259ee38f37f3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-25 18:45:28 +0530

    Add blog creation form

--- part5/bloglist/frontend/src/components/Blogs.js
+++ part5/bloglist/frontend/src/components/Blogs.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Blog from './Blog'
 
-const Blogs = ({ blogs, user, handleLogout }) => {
+const Blogs = ({ blogs, user, handleLogout, handleCreate }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   return (
     <div>
       <h2>blogs</h2>
@@ -10,6 +14,49 @@ const Blogs = ({ blogs, user, handleLogout }) => {
         <button onClick={handleLogout}>logout</button>
       </div>
       <br />
+      <h2>create new</h2>
+      <form
+        onSubmit={async (e) => {
+          e.preventDefault()
+          await handleCreate({ title, author, url })
+        }}
+      >
+        {/* title */}
+        <label htmlFor='title'>title:</label>
+        <input
+          type='text'
+          id='title'
+          name='title'
+          value={title}
+          onChange={({ target: { value } }) => setTitle(value)}
+        />
+        <br />
+
+        {/* author */}
+        <label htmlFor='author'>author:</label>
+        <input
+          type='text'
+          id='author'
+          name='author'
+          value={author}
+          onChange={({ target: { value } }) => setAuthor(value)}
+        />
+        <br />
+
+        {/* url */}
+        <label htmlFor='url'>url:</label>
+        <input
+          type='text'
+          id='url'
+          name='url'
+          value={url}
+          onChange={({ target: { value } }) => setUrl(value)}
+        />
+        <br />
+
+        <button type='submit'>create</button>
+      </form>
+
       <div>
         {blogs.map((blog) => (
           <Blog key={blog.id} blog={blog} />
