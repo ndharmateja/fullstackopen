154be2d726633f6d10c175b8f4e3581165f5336b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-25 16:49:35 +0530

    Add logging middleware

--- part5/bloglist/backend/app.js
+++ part5/bloglist/backend/app.js
@@ -20,6 +20,7 @@ mongoose
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
--- part5/bloglist/backend/utils/middleware.js
+++ part5/bloglist/backend/utils/middleware.js
@@ -36,4 +36,18 @@ const userExtractor = (request, response, next) => {
   next()
 }
 
-module.exports = { unknownRoute, errorHandler, tokenExtractor, userExtractor }
+const requestLogger = (request, response, next) => {
+  info('Method:', request.method)
+  info('Path:  ', request.path)
+  info('Body:  ', request.body)
+  info('---')
+  next()
+}
+
+module.exports = {
+  unknownRoute,
+  errorHandler,
+  tokenExtractor,
+  userExtractor,
+  requestLogger,
+}
