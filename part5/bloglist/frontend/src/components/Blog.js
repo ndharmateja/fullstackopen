f4a45d23b8a24625a7c2b6da6d9a4fb5158a4f87
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-05-27 14:19:22 +0530

    Exercise 5.7

--- part5/bloglist/frontend/src/components/Blog.js
+++ part5/bloglist/frontend/src/components/Blog.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+const Blog = ({ blog }) => {
+  const [showFull, setShowFull] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  const toggleShowFull = () => setShowFull((oldShowFull) => !oldShowFull)
+
+  console.log(JSON.stringify(blog, null, 2))
+  return (
+    <div style={blogStyle}>
+      <div>
+        <span>
+          {blog.title} - {blog.author}
+        </span>
+        <button onClick={toggleShowFull}>{showFull ? 'hide' : 'view'}</button>
+        {showFull && (
+          <div>
+            <span>{blog.url}</span>
+            <br />
+            <span>
+              likes: {blog.likes} <button>like</button>
+            </span>
+            <br />
+            <span>{blog.user.name}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
