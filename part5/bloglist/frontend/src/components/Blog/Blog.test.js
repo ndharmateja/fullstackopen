0087a1d2738fc97c79188f5c13dee111aca727f8
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-03 22:31:25 +0530

    Exercise 5.14

--- part5/bloglist/frontend/src/components/Blog/Blog.test.js
+++ part5/bloglist/frontend/src/components/Blog/Blog.test.js
@@ -1,7 +1,8 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import Blog from './Blog'
+import userEvent from '@testing-library/user-event'
 
 test('blog renders title and author but not likes or url', () => {
   const blog = {
@@ -29,3 +30,36 @@ test('blog renders title and author but not likes or url', () => {
   expect(blogDiv).not.toHaveTextContent(blog.url)
   expect(blogDiv).not.toHaveTextContent(blog.likes)
 })
+
+test('url and likes shown after clicking view', async () => {
+  const blog = {
+    title: 'Some blog title',
+    author: 'some author',
+    url: 'some url',
+    likes: 23,
+    user: { name: 'Creator' },
+  }
+
+  const handleUpdate = jest.fn()
+  const handleDelete = jest.fn()
+  const { container } = render(
+    <Blog
+      blog={blog}
+      isCreatedByCurrentUser={true}
+      handleDelete={handleDelete}
+      handleUpdate={handleUpdate}
+    />
+  )
+
+  const user = userEvent.setup()
+  const viewButton = screen.getByText('view')
+  await user.click(viewButton)
+
+  const blogDiv = container.querySelector('.blog')
+
+  expect(blogDiv).toHaveTextContent(blog.title)
+  expect(blogDiv).toHaveTextContent(blog.author)
+  expect(blogDiv).toHaveTextContent(blog.url)
+  expect(blogDiv).toHaveTextContent(blog.likes)
+})
+
