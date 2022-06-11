51eb3e5258e697cb24b69c4f36a76ca740f9a105
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-11 19:02:19 +0530

    Exercise 6.3

--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -20,10 +20,27 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.data.id
+      const anecdoteToChange = state.find((a) => a.id === id)
 
-  return state
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1,
+      }
+      return state.map((anecdote) =>
+        anecdote.id !== id ? anecdote : changedAnecdote
+      )
+    }
+    default:
+      return state
+  }
 }
+
+export const increaseVote = (id) => ({
+  type: 'VOTE',
+  data: { id },
+})
 
 export default reducer
