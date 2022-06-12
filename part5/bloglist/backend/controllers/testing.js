e0750c613fd77190c5d363fdab676d0b3c71b560
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-12 14:31:13 +0530

    Return response on reset route

--- part5/bloglist/backend/controllers/testing.js
+++ part5/bloglist/backend/controllers/testing.js
@@ -4,6 +4,8 @@ const User = require('../models/user')
 const resetDb = async (request, response) => {
   await User.deleteMany({})
   await Blog.deleteMany({})
+
+  response.status(204).end()
 }
 
 module.exports = resetDb
