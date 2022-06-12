ca5570d92fe33778e805a7eb28122b399cfacef7
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-12 16:08:42 +0530

    Exercise 5.19

--- part5/bloglist/frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist/frontend/cypress/integration/blog_app.spec.js
@@ -40,4 +40,31 @@ describe('Blog app', function () {
       cy.contains('logout')
     })
   })
+
+  describe('When logged in', function () {
+    beforeEach(function () {
+      cy.login({ username: 'dharma', password: 'pass' })
+      cy.createBlog({ title: 'title1', author: 'author1', url: 'url1' })
+    })
+
+    it.only('A blog can be created', function () {
+      cy.contains('Create New Blog').click()
+      cy.get('#title').type('New title')
+      cy.get('#author').type('New author')
+      cy.get('#url').type('New url')
+      cy.get('#create-blog-button').click()
+
+      cy.get('.notification')
+        .should('contain', 'a new blog')
+        .should('contain', 'added')
+        .should('contain', '"New title"')
+        .should('contain', 'New author')
+        .and('have.css', 'color', 'rgb(0, 128, 0)')
+        .and('have.css', 'border-style', 'solid')
+        .and('have.css', 'border-color', 'rgb(0, 128, 0)')
+
+      cy.get('#blogs').contains('"New title"')
+      cy.get('#blogs').contains('New author')
+    })
+  })
 })
