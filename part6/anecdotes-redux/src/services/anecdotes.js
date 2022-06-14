6dc31a9cb64c46d67530e423868939a2829db990
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-15 05:22:03 +0530

    Disable eslint warning

--- part6/anecdotes-redux/src/services/anecdotes.js
+++ part6/anecdotes-redux/src/services/anecdotes.js
@@ -13,4 +13,5 @@ const createAnecdote = async (content) => {
   return response.data
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAnecdotes, createAnecdote }
