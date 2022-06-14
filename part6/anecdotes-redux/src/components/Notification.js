50fe665befde04d99c777beb6c41ae9b2a71c4d8
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-15 04:14:29 +0530

    Add removeNotification action

--- part6/anecdotes-redux/src/components/Notification.js
+++ part6/anecdotes-redux/src/components/Notification.js
@@ -1,13 +1,13 @@
 import { useSelector } from 'react-redux'
 
 const Notification = () => {
-  const notification = useSelector((state) => state.notification)
+  const { show, notification } = useSelector((state) => state.notification)
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
   }
-  return <div style={style}>{notification}</div>
+  return show ? <div style={style}>{notification}</div> : null
 }
 
 export default Notification
--- part6/anecdotes-redux/src/redux/reducers/notificationReducer.js
+++ part6/anecdotes-redux/src/redux/reducers/notificationReducer.js
@@ -1,16 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit'
 
-const initialState = 'notification'
+const initialState = { notification: 'notification', show: false }
 
 const notificationSlice = createSlice({
   name: 'notification',
   initialState,
   reducers: {
     setNotification(state, action) {
-      return action.payload
+      return { ...state, notification: action.payload }
+    },
+    removeNotification(state) {
+      return { ...state, show: false }
     },
   },
 })
 
-export const { setNotification } = notificationSlice.actions
+export const { setNotification, removeNotification } = notificationSlice.actions
 export default notificationSlice.reducer
