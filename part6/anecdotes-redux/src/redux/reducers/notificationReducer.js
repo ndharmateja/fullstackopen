1cec560f8941db31d0d0c09328539b05a45c3413
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-15 04:10:34 +0530

    Add notification reducer

---
+++ part6/anecdotes-redux/src/redux/reducers/notificationReducer.js
@@ -0,0 +1,16 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = 'notification'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState,
+  reducers: {
+    updateNotification(state, action) {
+      return action.payload
+    },
+  },
+})
+
+export const { updateNotification } = notificationSlice.actions
+export default notificationSlice.reducer
