6fbead6dd85df20685296f7acb98b8a5b656502f
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-15 05:14:10 +0530

    Fix bug of port

--- part6/anecdotes-redux/src/services/anecdotes.js
+++ part6/anecdotes-redux/src/services/anecdotes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3000/anecdotes'
+const baseUrl = 'http://localhost:3001/anecdotes'
 
 const getAnecdotes = async () => {
   const response = await axios.get(baseUrl)
