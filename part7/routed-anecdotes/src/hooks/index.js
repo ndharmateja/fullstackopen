c838d9c9f1fddaf45b3ed6790e64e1486a1f0a38
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-21 10:19:50 +0530

    Add reset function to useField

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -3,5 +3,6 @@ import { useState } from 'react'
 export const useField = (type) => {
   const [value, setValue] = useState('')
   const onChange = ({ target: { value } }) => setValue(value)
-  return { type, value, onChange }
+  const reset = () => setValue('')
+  return { type, value, onChange, reset }
 }
