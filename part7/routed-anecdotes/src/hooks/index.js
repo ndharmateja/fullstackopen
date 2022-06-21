75b36a0c30aa9cb753d6b47465a1a60160ec3325
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-06-21 10:27:41 +0530

    Exercise 7.5

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -4,5 +4,5 @@ export const useField = (type) => {
   const [value, setValue] = useState('')
   const onChange = ({ target: { value } }) => setValue(value)
   const reset = () => setValue('')
-  return { type, value, onChange, reset }
+  return { input: { type, value, onChange }, reset }
 }
