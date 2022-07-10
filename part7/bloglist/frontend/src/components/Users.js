a9f8ca00e0155b98b542f08e85667be30f848d15
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-10 22:02:14 +0530

    Exercise 7.14

--- part7/bloglist/frontend/src/components/Users.js
+++ part7/bloglist/frontend/src/components/Users.js
@@ -1,7 +1,31 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import User from './User'
 
 const Users = () => {
-  return <h1>Users</h1>
+  const users = useSelector((state) => state.users)
+
+  const component =
+    users.length === 0 ? (
+      <div>No Users</div>
+    ) : (
+      <table style={{ textAlign: 'left' }}>
+        <tr>
+          <th></th>
+          <th>blogs created</th>
+        </tr>
+        {users.map((user) => (
+          <User key={user.id} user={user} />
+        ))}
+      </table>
+    )
+
+  return (
+    <>
+      <h2>Users</h2>
+      {component}
+    </>
+  )
 }
 
 export default Users
