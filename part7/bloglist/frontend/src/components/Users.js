7321f4ff0aecb735e2d9f8b9b985da43caf2d0fd
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-10 22:07:51 +0530

    Add link to user name

--- part7/bloglist/frontend/src/components/Users.js
+++ part7/bloglist/frontend/src/components/Users.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import User from './User'
+import { Link } from 'react-router-dom'
 
 const Users = () => {
   const users = useSelector((state) => state.users)
@@ -10,13 +10,22 @@ const Users = () => {
       <div>No Users</div>
     ) : (
       <table style={{ textAlign: 'left' }}>
-        <tr>
-          <th></th>
-          <th>blogs created</th>
-        </tr>
-        {users.map((user) => (
-          <User key={user.id} user={user} />
-        ))}
+        <thead>
+          <tr>
+            <th></th>
+            <th>blogs created</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user.id}>
+              <td>
+                <Link to={`/users/${user.id}`}>{user.name}</Link>
+              </td>
+              <td>{user.blogs.length}</td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     )
 
