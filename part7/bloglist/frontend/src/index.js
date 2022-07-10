a4d0cf153ed5c775d1e78c4b6e0bbb0538d49eb3
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-10 07:46:46 +0530

    Add user to redux store

--- part7/bloglist/frontend/src/index.js
+++ part7/bloglist/frontend/src/index.js
@@ -5,11 +5,13 @@ import App from './App'
 import notificationReducer from './reducers/notificationReducer'
 import blogsReducer from './reducers/blogsReducer'
 import { Provider } from 'react-redux'
+import userReducer from './reducers/userReducer'
 
 const store = configureStore({
   reducer: {
     notification: notificationReducer,
     blogs: blogsReducer,
+    user: userReducer,
   },
 })
 
