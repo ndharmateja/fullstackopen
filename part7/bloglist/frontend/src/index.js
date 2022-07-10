5b982c1b543d2009c54101b6369ca4f6d4e160c4
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-10 06:40:30 +0530

    Add store provider to the app

--- part7/bloglist/frontend/src/index.js
+++ part7/bloglist/frontend/src/index.js
@@ -1,5 +1,18 @@
+import { configureStore } from '@reduxjs/toolkit'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import notificationReducer from './reducers/notificationReducer'
+import { Provider } from 'react-redux'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const store = configureStore({
+  reducer: {
+    notification: notificationReducer,
+  },
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
