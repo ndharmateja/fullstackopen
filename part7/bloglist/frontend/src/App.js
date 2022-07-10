238f92114e16a642238a5776d0b2e9a2b11c89b2
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-10 19:43:58 +0530

    Refactor into Blogs component

--- part7/bloglist/frontend/src/App.js
+++ part7/bloglist/frontend/src/App.js
@@ -1,30 +1,20 @@
-import { useEffect, useRef } from 'react'
-import Blog from './components/Blog/Blog'
-import BlogForm from './components/BlogForm/BlogForm'
+import { useEffect } from 'react'
 import Notification from './components/Notification'
-import Togglable from './components/Togglable'
 import LoginForm from './components/LoginForm'
 import blogService from './services/blogs'
 import { useDispatch, useSelector } from 'react-redux'
-import {
-  createBlog,
-  deleteBlog,
-  initializeBlogs,
-  updateBlog,
-} from './reducers/blogsReducer'
+import { initializeBlogs } from './reducers/blogsReducer'
 import {
   loginUser,
   logoutUser,
   loadUserFromStorage,
 } from './reducers/userReducer'
+import Blogs from './components/Blogs/Blogs'
 
 const App = () => {
   const dispatch = useDispatch()
 
   const { notification, user } = useSelector((state) => state)
-  const blogs = [...useSelector((state) => state.blogs)]
-
-  const blogFormRef = useRef()
 
   useEffect(() => {
     dispatch(initializeBlogs(blogService))
@@ -46,22 +36,6 @@ const App = () => {
     dispatch(logoutUser())
   }
 
-  const handleCreate = async ({ title, author, url }) => {
-    dispatch(createBlog({ title, author, url }))
-
-    // Hide the create form
-    blogFormRef.current.toggleVisibility()
-  }
-
-  const handleDelete = async (id) => {
-    dispatch(deleteBlog(id))
-  }
-
-  const handleUpdate = async ({ id, title, author, url, likes }) => {
-    dispatch(updateBlog({ id, title, author, url, likes }))
-  }
-
-  blogs.sort((blog1, blog2) => blog2.likes - blog1.likes)
   return (
     <>
       {!user && <LoginForm handleLogin={handleLogin} />}
@@ -74,21 +48,7 @@ const App = () => {
             <button onClick={handleLogout}>logout</button>
           </div>
           <br />
-          <Togglable buttonLabel='Create New Blog' ref={blogFormRef}>
-            <BlogForm handleCreate={handleCreate} />
-          </Togglable>
-
-          <div id='blogs'>
-            {blogs.map((blog) => (
-              <Blog
-                key={blog.id}
-                blog={blog}
-                handleUpdate={handleUpdate}
-                handleDelete={handleDelete}
-                isCreatedByCurrentUser={blog.user.username === user.username}
-              />
-            ))}
-          </div>
+          <Blogs />
         </div>
       )}
     </>
---
+++ part7/bloglist/frontend/src/components/Blogs/Blogs.js
@@ -0,0 +1,62 @@
+import { useEffect, useRef } from 'react'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import Blog from '../Blog/Blog'
+import Togglable from '../Togglable'
+import {
+  createBlog,
+  deleteBlog,
+  initializeBlogs,
+  updateBlog,
+} from '../../reducers/blogsReducer'
+import BlogForm from '../BlogForm/BlogForm'
+
+const Blogs = () => {
+  const dispatch = useDispatch()
+
+  const blogs = [...useSelector((state) => state.blogs)]
+  const user = useSelector((state) => state.user)
+
+  const blogFormRef = useRef()
+
+  useEffect(() => {
+    dispatch(initializeBlogs())
+  }, [dispatch])
+
+  const handleCreate = async ({ title, author, url }) => {
+    dispatch(createBlog({ title, author, url }))
+
+    // Hide the create form
+    blogFormRef.current.toggleVisibility()
+  }
+
+  const handleDelete = async (id) => {
+    dispatch(deleteBlog(id))
+  }
+
+  const handleUpdate = async ({ id, title, author, url, likes }) => {
+    dispatch(updateBlog({ id, title, author, url, likes }))
+  }
+
+  blogs.sort((blog1, blog2) => blog2.likes - blog1.likes)
+  return (
+    <>
+      <Togglable buttonLabel='Create New Blog' ref={blogFormRef}>
+        <BlogForm handleCreate={handleCreate} />
+      </Togglable>
+      <div id='blogs'>
+        {blogs.map((blog) => (
+          <Blog
+            key={blog.id}
+            blog={blog}
+            handleUpdate={handleUpdate}
+            handleDelete={handleDelete}
+            isCreatedByCurrentUser={blog.user.username === user.username}
+          />
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default Blogs
