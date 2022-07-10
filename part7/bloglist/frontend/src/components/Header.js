05cea2d07351308cc5635134c2c57e751a396c08
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-10 23:23:57 +0530

    Exercise 7.17

--- part7/bloglist/frontend/src/components/Header.js
+++ part7/bloglist/frontend/src/components/Header.js
@@ -1,10 +1,38 @@
 import React from 'react'
-import LoggedUser from './LoggedUser'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+import { logoutUser } from '../reducers/userReducer'
 
 const Header = () => {
+  const style = { margin: '0.25rem' }
+  const dispatch = useDispatch()
+  const user = useSelector((state) => state.user)
+
+  const handleLogout = () => {
+    dispatch(logoutUser())
+  }
   return (
     <>
-      <LoggedUser />
+      <nav
+        style={{
+          listStyle: 'none',
+          margin: 0,
+          padding: 0,
+          display: 'flex',
+          backgroundColor: '#ccc',
+        }}
+      >
+        <li style={style}>
+          <Link to='/'>blogs</Link>
+        </li>
+        <li style={style}>
+          <Link to='/users'>users</Link>
+        </li>
+        <div style={style} className=''>
+          <span>{user.name} logged in </span>
+          <button onClick={handleLogout}>logout</button>
+        </div>
+      </nav>
       <h2>blog app</h2>
     </>
   )
