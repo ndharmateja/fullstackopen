3289c687a147394426a76b4ae420ac0ada9f6c6b
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-11 07:51:35 +0530

    Add likes and comments to blog item

--- part7/bloglist/frontend/src/components/Blogs.js
+++ part7/bloglist/frontend/src/components/Blogs.js
@@ -2,9 +2,10 @@ import { useRef } from 'react'
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import Togglable from './Togglable'
-import { createBlog } from '../reducers/blogsReducer'
+import { createBlog, updateBlog } from '../reducers/blogsReducer'
 import BlogForm from './BlogForm/BlogForm'
 import { Link } from 'react-router-dom'
+import { FaHeart, FaComment } from 'react-icons/fa'
 
 const Blogs = () => {
   const dispatch = useDispatch()
@@ -24,9 +25,21 @@ const Blogs = () => {
     border: '1px solid black',
     margin: '0.5rem',
     padding: '0.5em',
+    display: 'flex',
+    justifyContent: 'space-between',
   }
 
   blogs.sort((blog1, blog2) => blog2.likes - blog1.likes)
+
+  const buttonStyle = {
+    outline: 'none',
+    background: 'none',
+    border: 'none',
+    cursor: 'pointer',
+    padding: 0,
+    margin: 0,
+  }
+
   return (
     <>
       <Togglable buttonLabel='Create New Blog' ref={blogFormRef}>
@@ -34,8 +47,25 @@ const Blogs = () => {
       </Togglable>
       <div id='blogs'>
         {blogs.map((blog) => (
-          <div style={blogItemStyle} key={blog.id}>
+          <div key={blog.id} style={blogItemStyle}>
             <Link to={`/blogs/${blog.id}`}>{blog.title}</Link>
+            <span>
+              {blog.likes}{' '}
+              <button
+                style={buttonStyle}
+                onClick={() =>
+                  dispatch(updateBlog({ ...blog, likes: blog.likes + 1 }))
+                }
+              >
+                <FaHeart />
+              </button>{' '}
+              {blog.comments.length}{' '}
+              <Link to={`/blogs/${blog.id}`}>
+                <button style={buttonStyle}>
+                  <FaComment />
+                </button>
+              </Link>
+            </span>
           </div>
         ))}
       </div>
