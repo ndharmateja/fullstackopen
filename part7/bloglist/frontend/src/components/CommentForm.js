0ac344d2a9551fa77533ea6881b82be91f267ba5
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-11 08:10:12 +0530

    Trim comment and set max length

--- part7/bloglist/frontend/src/components/CommentForm.js
+++ part7/bloglist/frontend/src/components/CommentForm.js
@@ -16,15 +16,15 @@ const CommentForm = ({ blogId }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    if (comment.value) {
-      dispatch(addComment({ id: blogId, content: comment.value }))
+    if (comment.value.trim()) {
+      dispatch(addComment({ id: blogId, content: comment.value.trim() }))
       setValue('')
     }
   }
 
   return (
     <form action='submit' onSubmit={handleSubmit}>
-      <textarea {...comment} placeholder='Add new comment' />
+      <textarea maxLength={100} {...comment} placeholder='Add new comment' />
       <br />
       <button type='submit'>add comment</button>
     </form>
