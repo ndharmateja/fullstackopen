21fd08869c8fa7184b1ddda76cef7768de562831
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-11 11:15:23 +0530

    Style notification with mui

--- part7/bloglist/frontend/src/components/Notification.js
+++ part7/bloglist/frontend/src/components/Notification.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
+import { Alert } from '@mui/material'
 
 const Notification = () => {
   const notification = useSelector((state) => state.notification)
@@ -8,21 +9,7 @@ const Notification = () => {
 
   const { message, isError } = notification
 
-  const color = isError ? 'red' : 'green'
-  const styles = {
-    padding: '20px',
-    backgroundColor: 'lightGrey',
-    color: color,
-    border: `5px solid ${color}`,
-    borderRadius: '10px',
-    margin: '20px 20px 20px 0px',
-    fontSize: '25px',
-  }
-  return (
-    <div className={isError ? 'error' : 'notification'} style={styles}>
-      {message}
-    </div>
-  )
+  return <Alert severity={isError ? 'error' : 'success'}>{message}</Alert>
 }
 
 export default Notification
