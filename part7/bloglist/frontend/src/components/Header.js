348e6b7d1be5f77c8b845cc0be9ef85ca201857c
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-11 11:25:32 +0530

    Style nav bar using mui

--- part7/bloglist/frontend/src/components/Header.js
+++ part7/bloglist/frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { logoutUser } from '../reducers/userReducer'
-import { Navbar, Nav } from 'react-bootstrap'
+import { AppBar, Toolbar, IconButton, Button } from '@mui/material'
 
 const Header = () => {
   const dispatch = useDispatch()
@@ -12,32 +12,29 @@ const Header = () => {
     dispatch(logoutUser())
   }
 
-  const padding = {
-    padding: 0,
-  }
   return (
     <>
-      <Navbar collapseOnSelect expand='md' bg='dark' variant='dark'>
-        <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-        <Navbar.Collapse id='responsive-navbar-nav'>
-          <Nav className='me-auto'>
-            <Nav.Link href='#' as='span'>
-              <Link style={padding} to='/'>
-                Blogs
-              </Link>
-            </Nav.Link>
-            <Nav.Link href='#' as='span'>
-              <Link style={padding} to='/users'>
-                Users
-              </Link>
-            </Nav.Link>
-          </Nav>
-          <div style={{ color: 'white' }} className='p-1'>
+      <AppBar position='static'>
+        <Toolbar>
+          <IconButton
+            edge='start'
+            color='inherit'
+            aria-label='menu'
+          ></IconButton>
+          <Button color='inherit' component={Link} to='/'>
+            blogs
+          </Button>
+          <Button color='inherit' component={Link} to='/users'>
+            users
+          </Button>
+          <div style={{ color: 'white', marginLeft: 'auto' }}>
             <span>{user.name} logged in </span>
-            <button onClick={handleLogout}>Logout</button>
+            <Button color='inherit' onClick={handleLogout}>
+              Logout
+            </Button>
           </div>
-        </Navbar.Collapse>
-      </Navbar>
+        </Toolbar>
+      </AppBar>
       <h2>blog app</h2>
     </>
   )
