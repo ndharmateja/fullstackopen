396eeaa37f9521297855baefe919a28dca7f485a
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-11 06:04:24 +0530

    Add comment model

---
+++ part7/bloglist/backend/models/comment.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+
+const commentSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  blog: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog',
+    required: true,
+  },
+})
+
+commentSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+const Comment = mongoose.model('Comment', commentSchema)
+
+module.exports = Comment
