0c0fd914f05da5d7c30eeeb1045911c5a4715536
Dharma Teja Nuli <n.dharmateja92@gmail.com>
2022-07-11 09:03:44 +0530

    Serve static files from backend

--- part7/bloglist/backend/app.js
+++ part7/bloglist/backend/app.js
@@ -22,6 +22,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(express.static('build'))
 
 app.use(middleware.tokenExtractor)
 app.use('/api/blogs', blogsRouter)
